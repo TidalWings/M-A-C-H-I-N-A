fe0e3473cfd8e3f72a11e62aa572fe5d0e6cd3a4
Warren Ngoun <steamb0t@users.noreply.github.com>
2019-02-13 20:53:50 -0800

    Messed around w/ Player Controllers
    Added a Cube, Plane, and messed with that all and the main camera.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed = 0.004f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () {
+		float deltaX = Input.GetAxis("Horizontal") * speed;
+		float deltaZ = Input.GetAxis("Vertical") * speed;
+
+		transform.Translate(deltaX, 0, deltaZ);
+	}
+}
