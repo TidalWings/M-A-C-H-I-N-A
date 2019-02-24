9cf4b6db9689dc4dea5fa8eaeec15bb0fbfef654
Warren Ngoun <steamb0t@users.noreply.github.com>
2019-02-24 01:30:32 -0800

    Updated Overworld
    Setup a mouse click controller and a working nav mesh for the Overworld, and a nav mesh agent for the Player.

---
+++ Assets/Scripts/ClickMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class ClickMove : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// If our click is a valid point on the Navmesh then, set that point as our destination
+		if (Input.GetButtonDown("Fire1")) {
+			// Gets a Ray from clicking
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit rayHit;
+			if (Physics.Raycast(ray, out rayHit)) {
+				// This takes the Point from the rayHit and sets it as our NavMesh Agent
+				// https://docs.unity3d.com/ScriptReference/AI.NavMeshAgent.html
+				GetComponent<NavMeshAgent>().SetDestination(rayHit.point);
+			}
+		}
+	}
+}
