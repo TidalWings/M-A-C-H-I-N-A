d795de2c21e2a183d653f95b2bded1e56ca7e1b7
Warren Ngoun <steamb0t@users.noreply.github.com>
2019-02-26 01:34:51 -0800

    Added ability to swap scenes
    Added ground prefab, added Isometric camera prefab, setup Test scene, setup ability to swap between scenes with O and P key.

---
+++ Assets/Scripts/SceneSwapTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// https://www.youtube.com/watch?v=Oadq-IrOazg
+
+public class SceneSwapTest : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("o")) {
+            SceneManager.LoadScene(0);
+        } else if (Input.GetKeyDown("p")) {
+            SceneManager.LoadScene(1);
+		}
+	}
+}
