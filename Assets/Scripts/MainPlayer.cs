afe58234c15e2767d968ec44190354f11cd9f448
Warren Ngoun <steamb0t@users.noreply.github.com>
2019-03-04 23:09:43 -0800

    BIG Update
    Made legacy scripts folder, redid certain scripts for actual game and not learning. Redid certain prefabs for collider compatability. Redid enemy prefab to path and get aggro from the Player. Setup tutorial areas and doors between each.

---
+++ Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MainPlayer : MonoBehaviour {
+	
+	void Update () {
+		// On RMB Click, Check if the point is a valid move, if yes then go there.
+		if (Input.GetMouseButtonDown(1)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit)) {
+                GetComponent<NavMeshAgent>().SetDestination(rayHit.point);
+            }
+		}
+	}
+}
---
+++ Assets/Scripts/NavigationSprite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationSprite : MonoBehaviour {
+
+    public GameObject nav_sprite;
+	
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+		// TODO: Sample the Nav Mesh to make sure it's a legal point
+        if (Physics.Raycast(ray, out rayHit) && Input.GetMouseButtonDown(1)) {
+			GameObject[] items = GameObject.FindGameObjectsWithTag("Sweets");
+			if (items.Length != 0) { foreach (GameObject _ in items) Destroy(_); }
+			// TODO: Instantiate w/out creating an empty var (compiler complains)
+			GameObject obj = Instantiate(nav_sprite, new Vector3(rayHit.point.x, (rayHit.point.y + 0.1f), rayHit.point.z), Quaternion.Euler(new Vector3(90, 0, 0))) as GameObject;
+        }
+	}
+
+    public void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Sweets(Clone)") {
+			Destroy(other.gameObject);
+		}
+    }
+}
