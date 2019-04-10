7116b29eb3484697055f3e2f76066ad1a46e7a08
Warren Ngoun <TidalWings@users.noreply.github.com>
2019-04-09 23:44:12 -0700

    Combined NavigationSprite and MainPlayer script, Fixed colliders on a few prefabs, Adjusted Layers for certain objects (so Raycasting doesn't hit their colliders)

--- Assets/Scripts/MainPlayer.cs
+++ Assets/Scripts/MainPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class MainPlayer : MonoBehaviour {
 	
+    public GameObject nav_sprite;
 	void Update () {
 		// On RMB Click, Check if the point is a valid move, if yes then go there.
 		if (Input.GetMouseButtonDown(1)) {
@@ -12,7 +13,20 @@ public class MainPlayer : MonoBehaviour {
             RaycastHit rayHit;
             if (Physics.Raycast(ray, out rayHit)) {
                 GetComponent<NavMeshAgent>().SetDestination(rayHit.point);
+                // Here we create an array of all Objs that are Clones of Sweets by checking for Tags
+                GameObject[] items = GameObject.FindGameObjectsWithTag("Sweets");
+                // This checks to make sure that if we have only 1 Sweets Nav Sprite at a time by deleting the rest
+                if (items.Length != 0) { foreach (GameObject _ in items) Destroy(_); }
+                Instantiate(nav_sprite, new Vector3(rayHit.point.x, (rayHit.point.y + 0.1f), rayHit.point.z), Quaternion.Euler(new Vector3(90, 0, 0)));
             }
 		}
 	}
+
+    // OnTriggerEnter checks for collision and we can grab the Other Obj or This
+    public void OnTriggerEnter(Collider other) {
+        // GameObjects have Names, so we can check what the other Obj is by checking either its name or its tag
+        if (other.gameObject.name == "Sweets(Clone)") {
+            Destroy(other.gameObject);
+        }
+    }
 }
