c2a0165e8f15b1267290320748865426a03cd492
Warren Ngoun <TidalWings@users.noreply.github.com>
2019-04-30 13:36:36 -0700

    Updated Box Worlds, Setup a Respawn Cube in Box World 02

---
+++ Assets/Scripts/RespawnCube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawnCube : MonoBehaviour {
+
+	[SerializeField] GameObject spawn_point;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Player") {
+			SceneManager.LoadScene("Box_World_02");
+		}
+	}
+}
