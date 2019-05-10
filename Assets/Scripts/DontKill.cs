1863eb5b4874d19a516cfd96d4873fd3f86a13c2
Warren Ngoun <TidalWings@users.noreply.github.com>
2019-05-10 16:22:03 -0700

    Added Scripts to Not Kill Background Music

---
+++ Assets/Scripts/DontKill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontKill : MonoBehaviour {
+    private static DontKill _instance = null;
+    public static DontKill Instance {
+        get { return _instance; }
+    }
+
+    void Awake() {
+        // PART OF THE SINGLETON PATTERN
+        if (_instance == null) {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+}
