b2852d002773595e17ddfe62b5f0f3b4a5dffe25
TidalWings <TidalWings@users.noreply.github.com>
2019-05-14 11:16:11 -0700

    Testing FrameRate

---
+++ Assets/Scripts/FrameRate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour {
+
+	public int mobile_framerate = 30;
+	public int pc_framerate = 60;
+
+	void Awake() {
+		QualitySettings.vSyncCount = 0;
+	}
+
+	void Update() {
+        if (Application.isMobilePlatform) {
+			if (mobile_framerate != Application.targetFrameRate) {
+				Application.targetFrameRate = mobile_framerate;
+			}
+        } else {
+			if (pc_framerate != Application.targetFrameRate) {
+				Application.targetFrameRate = pc_framerate;
+			}
+        }
+	}
+}
