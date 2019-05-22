ad1c4b4c0a539d518cf2ea81e6e1a2d6da8b3f77
ethberman <47404163+ethberman@users.noreply.github.com>
2019-05-22 14:49:20 -0700

    Added Ending

---
+++ Assets/Scripts/Dialogue/DialogueControllerSpecial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//must be attached to object that triggers dialogue
+public class DialogueControllerSpecial : MonoBehaviour {
+	[SerializeField] private GameObject textbox;  //popup window for dialogue
+	//popup must have: button, text(name), text(sentence)
+	public string scene_name;
+
+	public Dialogue dialogue;
+	public void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "Player") {
+			//while count < TextBox.size()
+			textbox.SetActive(true);  //opens popup
+			//i++
+			Debug.Log("talk");
+			FindObjectOfType<DialogueManagerSpecial> ().StartDialogue (dialogue);
+		}
+	}
+	public void Ender() {
+		textbox.SetActive (false); //closed popup called by Manager when queue is empty
+
+		gameObject.SetActive (false); // deactivates dialogue trigger
+		SceneManager.LoadScene(scene_name);
+
+	}
+}
---
+++ Assets/Scripts/Dialogue/DialogueManagerSpecial.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManagerSpecial : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	private Queue<string> info;
+	// Use this for initialization
+	void Start () {
+		info = new Queue<string> ();
+	}
+
+	public void StartDialogue (Dialogue dialogue) {
+		Debug.Log ("start" + dialogue.name);
+
+		nameText.text = dialogue.name;
+
+		info.Clear ();
+		foreach (string sentence in dialogue.sentences) {
+			info.Enqueue (sentence);
+		}
+		DisplayNextSentence ();
+	}
+	public void DisplayNextSentence ()
+	{
+		if (info.Count == 0)
+		{
+			EndDialogue ();
+			return;
+		}
+
+		string sentence = info.Dequeue ();
+		dialogueText.text = sentence;
+	}
+	void EndDialogue() {
+		FindObjectOfType<DialogueControllerSpecial> ().Ender (); 
+		Debug.Log ("End of Dialogue");
+	}
+}
